d991ed671eb1243e4a43073ddbe294fa08f7ab2d
Andreas Karatzas <andreas.karatzas@protonmail.com>
2021-08-01 13:25:30 +0300

    Update clock.v

--- clock.v
+++ clock.v
@@ -35,21 +35,18 @@ module TwentyFiveMHertz ( reset, clk, pulse );
 endmodule
 
 /*
-
 In this example, we are going to use this clock divider to implement a signal of exactly .5 Hz frequency. 
 First, we will need to calculate the constant. 
 As an example, the input clock frequency of the Spartan3 is 100 MHz. We want our `flash` to be .5 Hz. 
 So it should take 200000000 clock cycles before `flash` goes to '1' and returns to '0'. 
 In another words, it takes 100000000 clock cycles for `flash` to flip its value. 
 So the constant we need to choose here is 100000000.
-
 */
-
 module HalfHertz( reset, clk, flash );
 
-	input  wire reset;
+    input  wire reset;
     input  wire clk;
-    output reg flash; 
+    output reg  flash; 
 
     parameter CYCLES = 100000000;
 
